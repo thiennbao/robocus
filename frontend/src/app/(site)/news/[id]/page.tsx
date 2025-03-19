ff2b620b8837a865712f9d233d3e558c78352c53
thiennbao <thiennbao.204@gmail.com>
2025-03-19 18:41:20 +0700

    add news article page layout

--- frontend/src/app/(site)/news/[id]/page.tsx
+++ frontend/src/app/(site)/news/[id]/page.tsx
@@ -1,10 +1,40 @@
-const NewsArticle = () => {
+import NewsArticle from "@/components/partials/newsArticle";
+import PageWall from "@/components/partials/pageWall";
+import Spacer from "@/components/partials/spacer";
+import { notFound } from "next/navigation";
 
-    return (
-        <div className="max-w-[74rem] mx-auto">
-            ihuv
-        </div>
-    )
-}
+const NewsArticlePage = ({ params, searchParams }: { params: { id: string }; searchParams: { search: string } }) => {
+  const news = newsMockData; // Fetch data with params
 
-export default NewsArticle;
+  if (!news) notFound();
+
+  return (
+    <main>
+      <PageWall title={news.title} image={news.thumbnail} />
+      <Spacer count={6} />
+      <NewsArticle news={news} search={searchParams.search} />
+      <Spacer count={6} />
+    </main>
+  );
+};
+
+export default NewsArticlePage;
+
+// Mock data
+const newsMockData = {
+  id: "1",
+  title: "ROBOCUS hợp tác với các trường đại học hàng đầu",
+  description:
+    "Chương trình hợp tác mới nhằm phát triển thế hệ kỹ sư robotics tương lai và thúc đẩy nghiên cứu đột phá.",
+  thumbnail: "https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png",
+  content: `
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+  `,
+  images: [],
+  date: new Date(),
+};
